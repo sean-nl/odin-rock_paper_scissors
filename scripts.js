deb2c171e068b3273002c59053c6efa46da624fc
sean-nl <sean.n.lamb.arch@gmail.com>
2024-03-11 20:59:37 -0400

    Add UI functionality
    Makes the Rock Paper Scissors game playable by clicking
    browser buttons.
    
    Control flow is now as follows:
    - User selects rock, paper, or scissors.
    - playRound() is called. After the score and round
    number are modified and displayed in-browser,
    playRound() calls checkWinCondition().
    - if checkWinCondition() determines the game is over,
    endGame() is called. This disables the r, p, s
    buttons and adds a "reset game" button to the DOM.
    - resetGame() clears all game history displayed in-browser,
    and resets the score and round counters.

--- scripts.js
+++ scripts.js
@@ -1,3 +1,105 @@
+const gameStats = { 
+    playerScore: 0,
+    computerScore: 0,
+    round: 0
+};
+
+//Get button and result region nodes
+const results = document.querySelector('#results');
+const rockBtn = document.querySelector('#rock');
+const paperBtn = document.querySelector('#paper');
+const scissorsBtn = document.querySelector('#scissors');
+
+//Named functions so that button event listeners can be added and removed
+function playRoundRock() { playRound('Rock', getComputerChoice()) };
+function playRoundPaper() { playRound('Paper', getComputerChoice()) };
+function playRoundScissors() { playRound('Scissors', getComputerChoice()) };
+
+resetGame();
+
+function resetGame() {
+    //Clear all displayed data for previous game
+    while (results.firstChild) {
+        results.removeChild(results.lastChild);
+    }
+
+    //Add listeners to buttons and style buttons to appear active
+    rockBtn.addEventListener('click', playRoundRock);
+    paperBtn.addEventListener('click', playRoundPaper);
+    scissorsBtn.addEventListener('click', playRoundScissors);
+
+    rockBtn.style.backgroundColor = 'white';
+    paperBtn.style.backgroundColor = 'white';
+    scissorsBtn.style.backgroundColor = 'white';
+
+    //Reset game stats
+    gameStats.playerScore = 0; 
+    gameStats.computerScore = 0;
+    gameStats.round = 0;
+}
+
+//Assumes playerSelection is 'Rock', 'Paper', or 'Scissors'.
+function playRound(playerSelection, computerSelection) {
+    const roundInfo = document.createElement('p');
+    gameStats.round++;
+    roundInfo.textContent = 'ROUND ' + gameStats.round;
+    results.appendChild(roundInfo);
+
+    const roundResult = document.createElement('p');
+
+    if ( 
+        playerSelection === 'Rock' && computerSelection === 'Scissors'
+        || playerSelection === 'Scissors' && computerSelection === 'Paper'
+        || playerSelection === 'Paper' && computerSelection === 'Rock'
+    ) {
+        gameStats.playerScore++;
+        roundResult.textContent = `You win! Your ${playerSelection} beats Computer's ${computerSelection}`;
+    }
+    else if (playerSelection === computerSelection) {
+        roundResult.textContent = `Tie! A deadlock between your ${playerSelection} and Computer's ${computerSelection}`;
+    }
+    else {
+        gameStats.computerScore++;
+        roundResult.textContent = `You lose! Your ${playerSelection} loses to Computer's ${computerSelection}`;
+    }
+    results.appendChild(roundResult);
+    checkWinCondition();
+}
+
+function checkWinCondition() {
+    const gameResult = document.createElement('p');
+
+    if (gameStats.playerScore === 3 || gameStats.computerScore === 3 || gameStats.round === 5) {
+        if (gameStats.playerScore === 3) {
+            gameResult.textContent = 'GAME COMPLETE! YOU WIN!';
+        } else if (gameStats.computerScore === 3) {
+            gameResult.textContent = 'GAME COMPLETE! YOU LOSE!';
+        } else {
+            gameResult.textContent = 'GAME COMPLETE! A THRILLING TIE!';
+        }
+
+        results.appendChild(gameResult);
+        endGame();
+    }
+}
+
+function endGame() {
+    //Create reset button
+    const resetBtn = document.createElement('button');
+    resetBtn.textContent = 'Start a new game';
+    resetBtn.addEventListener('click', () => resetGame());
+    results.appendChild(resetBtn);
+
+    //Disable action buttons and style accordingly
+    rockBtn.removeEventListener('click', playRoundRock);
+    paperBtn.removeEventListener('click', playRoundPaper);
+    scissorsBtn.removeEventListener('click', playRoundScissors);
+
+    rockBtn.style.backgroundColor = 'grey';
+    paperBtn.style.backgroundColor = 'grey';
+    scissorsBtn.style.backgroundColor = 'grey';
+}
+
 function getComputerChoice() {
     const choices = ['Rock', 'Paper', 'Scissors'];
     return choices[Math.floor(Math.random() * choices.length)];
@@ -5,76 +107,4 @@ function getComputerChoice() {
 
 function capitalize(text) {
     return text.slice(0,1).toUpperCase() + text.slice(1).toLowerCase();
-}
-
-//Assumes playerSelection is 'Rock', 'Paper', or 'Scissors'.
-//Returns: 1 = one point for player, 0 = tie, -1 = one point for computer
-function playRound(playerSelection, computerSelection) {
-    if ( 
-        playerSelection === 'Rock' && computerSelection === 'Scissors'
-        || playerSelection === 'Scissors' && computerSelection === 'Paper'
-        || playerSelection === 'Paper' && computerSelection === 'Rock'
-    ) {
-        return 1;
-    }
-    else if (playerSelection === computerSelection) {
-        return 0;
-    }
-    else {
-        return -1;
-    }
-}
-
-function playGame() {
-    //Play a five-round game and report a winner or loser at the end.
-    let computerScore = 0;
-    let playerScore = 0;
-
-    console.log("Let's play a five-round game of Rock Paper Scissors!");
-    
-    for (let i = 0; i < 5; i++) {
-        let playerSelection = prompt("What shape do you want to pick? (Rock, Paper, or Scissors)");
-        
-        playerSelection = capitalize(playerSelection); // Case-insensitive string handling
-        computerSelection = getComputerChoice();
-        
-        let result = playRound(playerSelection, computerSelection);
-
-        console.log('ROUND ' + (i+1));
-        
-        if (result === 1) {
-            playerScore++;
-            console.log(`You win! Your ${playerSelection} beats Computer's ${computerSelection}`);
-        }
-        else if (result === -1) {
-            computerScore++;
-            console.log(`You lose! Your ${playerSelection} loses to Computer's ${computerSelection}`);
-        }
-        else {
-            console.log(`Tie! A deadlock between your ${playerSelection} and Computer's ${computerSelection}`);
-        }
-
-        console.log(`The score is now: You - ${playerScore} Computer - ${computerScore}`);
-        
-        //If the player or the computer gets three points, they have won.
-        if (playerScore >= 3 || computerScore >= 3) {
-            break;
-        }
-
-    }
-
-    console.log('GAME COMPLETE!');
-
-    if (playerScore > computerScore) {
-        console.log(`YOU WIN!`);
-    }
-    else if (playerScore < computerScore) {
-        console.log(`YOU LOSE!`);
-    }
-    else {
-        console.log(`A THRILLING TIE!`);
-    }
-
-    console.log(`Final score: You - ${playerScore} Computer - ${computerScore}`);
-
 }
